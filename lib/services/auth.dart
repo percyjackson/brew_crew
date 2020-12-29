1b3063512f071cfea83f480830cc0606251e56d4
Yeison Osorio <yeison-arley25@hotmail.com>
2020-12-29 16:40:58 -0500

    Auth change user stream

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,14 +1,28 @@
+import 'package:brew_crew/models/user.dart' as userModel;
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //create user obj based on Firebase User
+  userModel.User _userFromFirebaseUser(User user) {
+    return user != null ? userModel.User(uid: user.uid) : null;
+  }
+
+  // auth change user stream
+  Stream<userModel.User> get user {
+    return _auth
+        .authStateChanges()
+        //.map((User user) => _userFromFirebaseUser(user));
+        .map(_userFromFirebaseUser);
+  }
 
   // sign in anon
   Future signInAnon() async {
     try {
       UserCredential result = await _auth.signInAnonymously();
       User user = result.user;
-      return user;
+      return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
       return null;
