aa21701b020bd552f43fd5459d503be3826501b8
Yeison Osorio <yeison-arley25@hotmail.com>
2021-04-12 18:00:50 -0500

    Sign in with mail and password

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -11,9 +11,11 @@ class SignIn extends StatefulWidget {
 
 class _SignInState extends State<SignIn> {
   final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
   // text field state
   String email = '';
   String password = '';
+  String error = '';
 
   @override
   Widget build(BuildContext context) {
@@ -36,10 +38,12 @@ class _SignInState extends State<SignIn> {
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
         child: Form(
+          key: _formKey,
           child: Column(
             children: <Widget>[
               SizedBox(height: 20.0),
               TextFormField(
+                validator: (val) => val.isEmpty ? 'Enter an email' : null,
                 onChanged: (val) {
                   setState(() {
                     email = val;
@@ -48,6 +52,8 @@ class _SignInState extends State<SignIn> {
               ),
               SizedBox(height: 20.0),
               TextFormField(
+                validator: (val) =>
+                    val.length < 6 ? 'Enter a password 6+ long' : null,
                 obscureText: true,
                 onChanged: (val) {
                   setState(() => password = val);
@@ -61,10 +67,33 @@ class _SignInState extends State<SignIn> {
                   style: TextStyle(color: Colors.white),
                 ),
                 onPressed: () async {
+                  /*
                   print(email);
                   print(password);
+                  dynamic result = await _auth.signInAnon();
+                  if (result == null) {
+                    print('Error signing in');
+                  } else {
+                    print('signed in');
+                    print(result.uid);
+                  }*/
+                  if (_formKey.currentState.validate()) {
+                    print(email);
+                    print(password);
+                    dynamic result =
+                        await _auth.signInWithEmailAndPassword(email, password);
+                    if (result == null) {
+                      setState(() =>
+                          error = 'Could not sign in with those credentials');
+                    }
+                  }
                 },
-              )
+              ),
+              SizedBox(height: 12.0),
+              Text(
+                error,
+                style: TextStyle(color: Colors.red, fontSize: 14),
+              ),
             ],
           ),
         ),
