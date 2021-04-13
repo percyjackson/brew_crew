7097648e4018a87ed91e7c5b167eeb8abd256887
Yeison Osorio <yeison-arley25@hotmail.com>
2021-04-13 18:14:46 -0500

    setting button and form

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,5 +1,6 @@
 import 'package:brew_crew/models/brew.dart';
 import 'package:brew_crew/screens/home/brew_list.dart';
+import 'package:brew_crew/screens/home/settings_form.dart';
 import 'package:brew_crew/services/auth.dart';
 import 'package:brew_crew/services/database.dart';
 import 'package:flutter/material.dart';
@@ -9,6 +10,17 @@ class Home extends StatelessWidget {
   final AuthService _auth = AuthService();
   @override
   Widget build(BuildContext context) {
+    void _showSettingsPanel() {
+      showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return Container(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 60),
+              child: SettingsForm(),
+            );
+          });
+    }
+
     return StreamProvider<List<Brew>>.value(
       value: DatabaseService().brews,
       child: Scaffold(
@@ -24,6 +36,11 @@ class Home extends StatelessWidget {
               onPressed: () async {
                 await _auth.signOut();
               },
+            ),
+            TextButton.icon(
+              onPressed: () => _showSettingsPanel(),
+              icon: Icon(Icons.settings),
+              label: Text('settings'),
             )
           ],
         ),
---
+++ lib/screens/home/settings_form.dart
@@ -0,0 +1,75 @@
+import 'package:brew_crew/shared/constants.dart';
+import 'package:flutter/material.dart';
+
+class SettingsForm extends StatefulWidget {
+  @override
+  _SettingsFormState createState() => _SettingsFormState();
+}
+
+class _SettingsFormState extends State<SettingsForm> {
+  final _formKey = GlobalKey<FormState>();
+  final List<String> sugars = ['0', '1', '2', '3', '4'];
+
+  // form values
+  String _currentName;
+  String _currentSugars;
+  int _currentStrength;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Update yor brew settings.',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 20),
+            TextFormField(
+              decoration: textInputDecoration,
+              validator: (val) => val.isEmpty ? 'Please enter a name' : null,
+              onChanged: (val) => setState(() => _currentName = val),
+            ),
+            SizedBox(height: 20),
+            // dropdown
+            DropdownButtonFormField(
+              decoration: textInputDecoration,
+              value: _currentSugars ?? '0',
+              onChanged: (val) => setState(() => _currentSugars = val),
+              items: sugars.map((sugar) {
+                return DropdownMenuItem(
+                  value: sugar,
+                  child: Text('$sugar sugars'),
+                );
+              }).toList(),
+            ),
+            // slider
+            Slider(
+              value: (_currentStrength ?? 100).toDouble(),
+              activeColor: Colors.brown[_currentStrength ?? 100],
+              inactiveColor: Colors.brown[_currentStrength ?? 100],
+              min: 100,
+              max: 900,
+              divisions: 8,
+              onChanged: (val) =>
+                  setState(() => _currentStrength = val.round()),
+            ),
+            ElevatedButton(
+              child: Text(
+                'update',
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () async {
+                print(_currentName);
+                print(_currentSugars);
+                print(_currentStrength);
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
