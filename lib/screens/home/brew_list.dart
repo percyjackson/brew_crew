b86537196fa371027acd7b4b063edb4a592eadd8
Yeison Osorio <yeison-arley25@hotmail.com>
2021-04-13 12:19:46 -0500

    Listing brews

--- lib/screens/home/brew_list.dart
+++ lib/screens/home/brew_list.dart
@@ -1,6 +1,9 @@
+import 'package:brew_crew/models/brew.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:provider/provider.dart';
+
+import 'brew_tile.dart';
 
 class BrewList extends StatefulWidget {
   @override
@@ -10,10 +13,17 @@ class BrewList extends StatefulWidget {
 class _BrewListState extends State<BrewList> {
   @override
   Widget build(BuildContext context) {
-    final brews = Provider.of<QuerySnapshot>(context);
-    for (var doc in brews.docs) {
-      print(doc.data());
-    }
-    return Container();
+    final brews = Provider.of<List<Brew>>(context);
+    brews.forEach((brew) {
+      print(brew.name);
+      print(brew.sugars);
+      print(brew.strength);
+    });
+    return ListView.builder(
+      itemCount: brews.length,
+      itemBuilder: (context, index) {
+        return BrewTile(brew: brews[index]);
+      },
+    );
   }
 }
