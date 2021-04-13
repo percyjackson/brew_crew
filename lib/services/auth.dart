dcb9c087c6fd947118c45df1f73ce9010b01af3b
Yeison Osorio <yeison-arley25@hotmail.com>
2021-04-12 22:34:30 -0500

    Database setup

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,4 +1,5 @@
 import 'package:brew_crew/models/user.dart' as userModel;
+import 'package:brew_crew/services/database.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
@@ -48,6 +49,9 @@ class AuthService {
       UserCredential result = await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
       User user = result.user;
+      //create a new document for the user with the uid
+      await DatabaseService(uid: user.uid)
+          .updateUserData('0', 'new member', 100);
       return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
